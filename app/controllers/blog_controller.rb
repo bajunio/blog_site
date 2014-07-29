633376b3a11bd93f7587726891ceb9e8a6960664
Brian <bajunio@gmail.com>
2014-07-28 23:14:10 -0700

    add comments view, linked by post_id and displayed on single_post.erb

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,12 +1,10 @@
 get '/' do
-  @post = Post.all
+  @posts = Post.all
   erb :index
 end
 
 get '/posts/:id' do |id|
-  @post = Post.find(id)
+  @post = Post.find_by_id(id)
+  @comments = Comment.where(post_id: @post.id)
   erb :single_post
 end
-
-
-
