1a1e8e5b17edcb100b9fa67a17ce21f452476395
Brian <bajunio@gmail.com>
2014-07-29 00:04:12 -0700

    add ability to comment on post with supporting erb

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -8,3 +8,15 @@ get '/posts/:id' do |id|
   @comments = Comment.where(post_id: @post.id)
   erb :single_post
 end
+
+get '/posts/:id/comments/new' do |id|
+  @post = Post.find_by_id(id)
+  erb :create_comment
+end
+
+post '/posts/:id/comment/new' do |id|
+  post = Post.find_by_id(id)
+  Comment.create(body: params[:body], post_id: post.id)
+  redirect "/posts/#{post.id}"
+end
+
